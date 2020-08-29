73eca255228584a95b6d671242955e975c518f78
Abhishek-Dhiman <abhishekdhiman2222@gmail.com>
2020-08-29 13:51:55 +0530

    changed security

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import environ
+import os
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
 # reading .env file
 environ.Env.read_env()
-import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
